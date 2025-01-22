d897398a258b269a7207c0418e40e9c09049d34d
Alexander Semenyuk <aww.semenyuk@gmail.com>
2025-01-22 11:36:27 +0300

    update middleware

--- AppliedPython/ht02/middleware.py
+++ AppliedPython/ht02/middleware.py
@@ -1,7 +1,11 @@
 from aiogram import BaseMiddleware
 from aiogram.types import Message
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 class LoggingMiddleware(BaseMiddleware):
     async def __call__(self, handler, event: Message, data: dict):
-        print(f"Получено сообщение: {event.text}")
+        logger.info(f"Получено сообщение: {event.text}")
         return await handler(event, data)
